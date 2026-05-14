f699df7a2a27e3a94b5656b04214e9cc8031f01f
DaniloOliv2 <dan.arauj.oliv@gmail.com>
2026-05-14 12:48:39 -0300

    Add files via upload

---
+++ database/database/supabase_config.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from supabase import create_client
+
+SUPABASE_URL = "https://dbtqvufidyjdhtpmtgvd.supabase.co"
+SUPABASE_KEY = "sb_publishable__jnla4vQuVQpiqmv0zUjHw_c2l4mBHG"
+
+def conectar_supabase():
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
