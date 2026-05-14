7fd193c29f7606f5210b1ece843bdb668a15bc68
DaniloOliv2 <dan.arauj.oliv@gmail.com>
2026-05-14 14:39:50 -0300

    Update supabase_config.py

--- database/supabase_config.py
+++ database/supabase_config.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from supabase import create_client
 
 SUPABASE_URL = "https://dbtqvufidyjdhtpmtgvd.supabase.co"
@@ -6,3 +5,5 @@ SUPABASE_KEY = "sb_publishable__jnla4vQuVQpiqmv0zUjHw_c2l4mBHG"
 
 def conectar_supabase():
     return create_client(SUPABASE_URL, SUPABASE_KEY)
+
+supabase = conectar_supabase()
