0687f7d1912b23b9f6b34247868c36b39f327184
DaniloOliv2 <dan.arauj.oliv@gmail.com>
2026-05-18 22:13:10 -0300

    Fix indentation for tela_investimentos call

--- app.py
+++ app.py
@@ -488,7 +488,7 @@ with aba9:
                     deletar_sonho(sonho["id"])
                     st.rerun()
     with aba10:
-    tela_investimentos(user_id)
+        tela_investimentos(user_id)
 
 with aba11:
     tela_exportar(user_id, mes)
